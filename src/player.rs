e355f5b5b34c00395f5fe0bb0c3ac516c867b6cd
Aapuji <79770468+Aapuji@users.noreply.github.com>
2023-05-06 16:27:00 -0400

    Update player.rs

--- src/player.rs
+++ src/player.rs
@@ -1,5 +1,27 @@
 use crate::card::Card;
 
 pub struct Player {
-  hand: Vec<Card>
+    name: String,
+    hand: Vec<Card>,
+}
+
+impl Player {
+    pub fn new(name: String) -> Self {
+        Self {
+            name,
+            hand: Vec::new(),
+        }
+    }
+
+    pub fn name(&self) -> &String {
+        &self.name
+    }
+
+    pub fn mut_hand(&mut self) -> &mut Vec<Card> {
+        &mut self.hand
+    }
+
+    pub fn hand(&self) -> &Vec<Card> {
+        &self.hand
+    }
 }
