3ef0c2093225976ca2b1f8427bfdb6d3f19f8f24
Aapuji <79770468+Aapuji@users.noreply.github.com>
2023-05-06 16:29:30 -0400

    Update main.rs (SciDev's changes)

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,26 @@
 use mao::card::{Card, Suit, Value};
 use mao::deck::Deck;
 use mao::game::Game;
+use mao::render::engine::{BoxDrawingProfile, TextFrameBuffer};
 
-fn main() {
+fn main() -> Result<(), Box<dyn std::error::Error>> {
     println!("Hello World");
 
-    let c = Card::new(Value::Ace, Suit::Spades);
+    // let c = Card::new(Value::Ace, Suit::Spades);
 
-    println!("{}", c);
+    // println!("{}", c);
 
-    let mut deck = Deck::default_52();
-    println!("{:#?}; {}", deck, deck.size());
-    deck.shuffle();
-    println!("SHUFFLED: {:#?}; {}", deck, deck.size());
+    // let mut deck = Deck::default_52();
+    // println!("{:#?}; {}", deck, deck.size());
+    // deck.shuffle();
+    // println!("SHUFFLED: {:#?}; {}", deck, deck.size());
+
+    let mut fb = TextFrameBuffer::new()?;
+
+    fb.fill_box(BoxDrawingProfile::SHADING[2], 2, 1, 4, 4)?;
+    fb.outline_box(BoxDrawingProfile::Normal, 2, 1, 4, 4)?;
+
+    println!("{}", fb.to_string());
+
+    Ok(())
 }
