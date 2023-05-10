58cd6cf8cfb0006586e37d740ca756b249e14d92
Aapuji <79770468+Aapuji@users.noreply.github.com>
2023-05-10 15:41:24 -0400

    Worked on gameplay loop
    In method `play`.

--- src/game.rs
+++ src/game.rs
@@ -1,23 +1,38 @@
 use crate::deck::Deck;
 use crate::player::Player;
+use std::sync::{Mutex, MutexGuard};
 
 #[derive(Debug)]
 pub struct Game {
     players: Vec<Player>,
     draw_pile: Deck,
     used_pile: Deck,
+    order: Order,
+    player_index: usize,
     game_over: bool,
     // rules: Vec<Rule> or HashSet<Rule> ?
 }
 
+// SciDev! Please make this description good. Idk if its right.
+/// The `Game` singleton that all the player computers reference.
+static GAME_SINGLETON: Mutex<Option<Game>> = Mutex::new(None);
+
 impl Game {
-    pub fn new(players: Vec<Player>) -> Self {
+    /// Takes a reference to the `Game` singleton in a thread-safe way.
+    pub fn instance<'a>() -> MutexGuard<'a, Option<Game>> {
+        GAME_SINGLETON.lock().unwrap()
+    }
+
+    pub fn begin(players: Vec<Player>) {
         let mut game = Self {
             players,
             draw_pile: Deck::default_52(),
             used_pile: Deck::empty(),
+            order: Order::Forward,
+            player_index: 0,
             game_over: false,
         };
+      
 
         // Deals 7 cards to each player
         // If there are less than 24 cards left, it adds another 52 cards to the deck
@@ -36,7 +51,7 @@ impl Game {
         // Starts game
         println!("Commence");
 
-        game
+        Self::instance() = game;
     }
 
     /// Checks size of `pile`. If it's smaller than `cmp`, then it appends a randomized `Deck`.
@@ -48,19 +63,92 @@ impl Game {
         }
     }
 
-    fn players(&self) -> &Vec<Player> {
+    pub fn next_player(&mut self) -> &Player {
+        self.player_index = match self.order {
+            Order::Forward => {
+                if self.player_index == self.num_players() - 1 {
+                    0
+                } else {
+                    self.player_index + 1
+                }
+            }
+
+            Order::Backward => {
+                if self.player_index == 0 {
+                    self.num_players() - 1
+                } else {
+                    self.player_index - 1
+                }
+            }
+        };
+
+        &self.players[self.player_index]
+    }
+
+    /// Goes through gameplay loop until a player wins, returns a ref to winning player
+    pub fn play(&mut self) -> &Player {
+        /*
+          Loop through each player
+            Player chooses between:
+              - playing card to used_pile
+                Chooses a card from their hand to play <in Player>
+              - taking card from draw_pile
+                Draws card from pile <in Player>
+        */
+        while !self.game_over {
+            let choose = true; // true = plays card, false = draws card
+            let current = self.current_player_mut();
+            if choose {
+                if current.num_cards() == 0 {
+                    self.game_over = true;
+                    break;
+                } else if self.current_player().num_cards() == 1 {
+                    println!("Mao!");
+                }
+                current.play_card(0, &mut self.used_pile);
+            } else {
+                self.current_player_mut().draw(&mut self.draw_pile);
+            }
+
+            self.next_player();
+        }
+
+        // while !self.game_over {
+        //     self.game_over = true;
+        // }
+
+        self.current_player()
+    }
+
+    pub fn current_player(&self) -> &Player {
+        &self.players[self.player_index]
+    }
+
+    pub fn current_player_mut(&mut self) -> &mut Player {
+        &mut self.players[self.player_index]
+    }
+
+    pub fn flip_order(&mut self) {
+        self.order = self.order.flip();
+    }
+
+    pub fn players(&self) -> &Vec<Player> {
         &self.players
     }
 
-    fn draw_pile(&self) -> &Deck {
+    pub fn num_players(&self) -> usize {
+        self.players.len()
+    }
+
+    pub fn draw_pile(&self) -> &Deck {
         &self.draw_pile
     }
 
-    fn used_pile(&self) -> &Deck {
+    pub fn used_pile(&self) -> &Deck {
         &self.used_pile
     }
 
-    fn game_over(&self) -> bool {
+    pub fn game_over(&self) -> bool {
         self.game_over
     }
 
@@ -74,5 +162,27 @@ impl Game {
         count += self.used_pile.size() as u32;
 
         count
+    }
+}
+
+#[derive(Debug)]
+pub enum Order {
+    Forward,
+    Backward,
+}
+
+impl Order {
+    fn flip(&self) -> Order {
+        match self {
+            Self::Forward => Self::Backward,
+            Self::Backward => Self::Forward,
+        }
+    }
+
+    fn val(&self) -> isize {
+        match self {
+            Self::Forward => 1,
+            Self::Backward => -1,
+        }
     }
 }
