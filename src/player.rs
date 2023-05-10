d8a34e2d124260a122cbbef1983fa84a73e6e87e
Aapuji <79770468+Aapuji@users.noreply.github.com>
2023-05-10 17:38:55 -0400

    Updated player.rs

--- src/player.rs
+++ src/player.rs
@@ -1,4 +1,5 @@
 use crate::card::Card;
+use crate::deck::Deck;
 
 #[derive(Debug)]
 pub struct Player {
@@ -14,6 +15,16 @@ impl Player {
         }
     }
 
+    pub fn draw(&mut self, deck: &mut Deck) -> Result<(), usize> {
+        deck.deal(1, self)
+    }
+
+    // Precondition: 0 <= card_index < self.hand
+    pub fn play_card(&mut self, card_index: usize, deck: &mut Deck) {
+        let card = self.hand.swap_remove(card_index);
+        deck.push_top(card);
+    }
+
     pub fn name(&self) -> &String {
         &self.name
     }
@@ -25,6 +36,10 @@ impl Player {
     pub fn hand(&self) -> &Vec<Card> {
         &self.hand
     }
+
+    pub fn num_cards(&self) -> usize {
+        self.hand.len()
+    }
 }
 
 #[macro_export]
@@ -33,7 +48,7 @@ macro_rules! players {
         vec![]
     };
 
-    ( $( $name:expr ),* ) => {
+    ( $( $name:expr ),* $(,)?) => {
       vec![$(
         mao::player::Player::new(
           String::from($name)
