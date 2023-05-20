355cca6716d2d04146c071aad063b36087715add
Aapuji <79770468+Aapuji@users.noreply.github.com>
2023-05-19 22:08:29 -0400

    Did some things + documentation

--- src/deck.rs
+++ src/deck.rs
@@ -5,49 +5,71 @@ use rand::seq::SliceRandom;
 use rand::thread_rng;
 use std::collections::VecDeque;
 use std::iter::IntoIterator;
+use std::ops::{Index, IndexMut};
 
+/// A struct representing a deck.
+/// Implemented as just a `VecDeque<mao::card::Card>`. 
 #[derive(Debug)]
 pub struct Deck {
     cards: VecDeque<Card>, // upside-down stack (last element is last card in stack)
 }
 
 impl Deck {
+    /// Creates a new `Deck` from `cards`.
     pub fn new(cards: VecDeque<Card>) -> Self {
         Self { cards }
     }
 
+    /// Returns a default, unshuffled `Deck` of 52 `Card`s.
     pub fn default_52() -> Self {
         Self {
             cards: all::<Card>().collect::<VecDeque<_>>(),
         }
     }
 
+    /// Returns an empty `Deck`.
     pub fn empty() -> Self {
         Self {
             cards: VecDeque::new(),
         }
     }
 
+    /// Randomizes the order of `Card`s in `self`.
     pub fn shuffle(&mut self) {
         self.cards.make_contiguous().shuffle(&mut thread_rng());
     }
 
+    /// Returns a reference to `self.cards`.
     pub fn cards(&self) -> &VecDeque<Card> {
         &self.cards
     }
 
+    /// Returns the size of the deck (# of cards).
     pub fn size(&self) -> usize {
         self.cards.len()
     }
 
+    /// Returns the top (front) `Card` of the deck.
+    pub fn top(&self) -> Card {
+        self[0]
+    }
+
+    /// Returns the bottom (back) `Card` of the deck.
+    pub fn bottom(&self) -> Card {
+        self[self.size() - 1]
+    }
+
+    /// Pushes `card` to the top of the deck.
     pub fn push_top(&mut self, card: Card) {
         self.cards.push_front(card);
     }
 
+    /// Pushes `card` to the bottom of the deck.
     pub fn push_bottom(&mut self, card: Card) {
         self.cards.push_back(card);
     }
 
+    /// Appends all the cards in `cards`, which is an iterator of `Card`s, to the bottom (back) of the deck.
     pub fn append<I>(&mut self, cards: I)
     where
         I: Iterator<Item = Card>,
@@ -55,6 +77,7 @@ impl Deck {
         self.cards.extend(cards);
     }
 
+    /// Prepends all the cards in `cards`, which is an iterator of `Card`s, to the top (front) of the deck.
     pub fn prepend<I>(&mut self, cards: I)
     where
         I: Iterator<Item = Card>,
@@ -64,7 +87,7 @@ impl Deck {
         }
     }
 
-    /// Attempts to deal `amt` of cards to `player`'s hand. If there aren't enough cards in the deck, it returns `Err` with the number of cards it didn't deal.
+    /// Attempts to deal `amt` cards to `player`'s hand. If there aren't enough cards in the deck, it returns `Err` with the number of cards it didn't deal.
     pub fn deal(&mut self, amt: usize, player: &mut Player) -> Result<(), usize> {
         if self.size() == 0usize {
             return Err(amt);
@@ -127,5 +150,19 @@ impl From<Vec<Card>> for Deck {
         Self {
             cards: VecDeque::from(cards),
         }
+    }
+}
+
+impl Index<usize> for Deck {
+    type Output = Card;
+
+    fn index(&self, index: usize) -> &Self::Output {
+        &self.cards[index]
+    }
+}
+
+impl IndexMut<usize> for Deck {
+    fn index_mut(&mut self, index: usize) -> &mut Self::Output {
+        &mut self.cards[index]
     }
 }
